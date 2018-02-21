e1c6fa7bf7a1fc7d4d37245df060477ca089934a
Chun-Fu Wang <chun-fu.wang@here.com>
2018-02-20 17:15:22 -0800

    Add more comments about working around newrelic-admin python installation requirements.

--- topology_builder/src/main/java/com/yelp/pyleus/PythonComponentsFactory.java
+++ topology_builder/src/main/java/com/yelp/pyleus/PythonComponentsFactory.java
@@ -11,8 +11,15 @@ import com.yelp.pyleus.spout.PythonSpout;
 public class PythonComponentsFactory {
 
     public static final String VIRTUALENV_INTERPRETER = "pyleus_venv/bin/python";
-    // The developer of the topology is responsible for putting a newrelic.ini at the root of the topology folder,
+    // A few notes on how this works:
+    // (1) The developer of the topology is responsible for putting a newrelic.ini at the root of the topology folder,
     // alongside with the topology definition file (.yml).
+    // (2) newrelic-admin only works when the python process it monitors uses the same python installation that
+    // installed newrelic-admin, as described here:
+    // https://docs.newrelic.com/docs/agents/python-agent/installation-configuration/python-agent-admin-script
+    // (3) newrelic-admin installed by Pyleus in the virtualenv actually points to the wrong python installation in a
+    // temporary folder. We work around that issue by run it with a specified python installation instead of
+    // running it directly as a self-executable script.
     public static final String NEW_RELIC_PREFIX = String.format("NEW_RELIC_CONFIG_FILE=newrelic.ini %s pyleus_venv/bin/newrelic-admin run-program", VIRTUALENV_INTERPRETER);
     public static final String MODULE_OPTION = "-m";
 
