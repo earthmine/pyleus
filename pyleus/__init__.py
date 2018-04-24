4d9c14c9df470be6ff544f2ad82985f37e582d80
Chun-Fu Wang <chun-fu.wang@here.com>
2018-04-23 17:34:04 -0700

    Bump version to 0.3.5

--- pyleus/__init__.py
+++ pyleus/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import pkg_resources
 
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 BASE_JAR = "pyleus-base.jar"
 BASE_JAR_PATH = pkg_resources.resource_filename('pyleus', BASE_JAR)
